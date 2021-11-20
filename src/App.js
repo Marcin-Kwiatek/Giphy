75af13737f291013adc4e393989cc443e0a5a81f
Marcin <kwiatekm4@gmail.com>
2021-11-20 13:30:42 +0100

    creating a site plan

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="title">Giphy Client</div>
+      <input></input>
+      <button style={{margin:'10px'}}>Search</button>
+      <div className="results">Results</div>
     </div>
   );
 }
